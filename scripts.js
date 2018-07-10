a492164441d748829d13face8ee9c47821c19429
melisamalala <melissamalala@gmail.com>
2018-07-10 15:05:35 +0300

    Added behaviour driven development

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  $("form#leap-year").sumbit(function(event) {
+  $("#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
-    console.log(year);
     var result = leapYear(year);
     $("#result").text(result);
+
   });
-});
+  });
 
 var leapYear = function(year) {
     return false;
