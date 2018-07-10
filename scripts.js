15ecdff947b9d5fb314d72b258ae8202aba6cf66
melisamalala <melissamalala@gmail.com>
2018-07-10 15:35:01 +0300

    simplified leap year function

--- scripts.js
+++ scripts.js
@@ -22,3 +22,25 @@ var leapYear = function(year) {
   };
 
   // jQuery user interface logic here.
+  var leapYear = function(year) {
+    if (year % 100 === 0) {
+      return false;
+    } else if (year % 4 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  // jQuery user interface logic here.
+
+
+  var leapYear = function(year) {
+      if ((year % 4 === 0) && (year % 100 !== 0)) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    // jQuery user interface logic here.
